b14442388d092d1e6c19281e44090604dcb3b6a7
Wu, Zhenyu <wuzhenyu@ustc.edu>
2023-06-20 23:37:36 +0800

    :memo: Update scripts/generate-requirements.md.pl

--- scripts/generate-requirements.md.pl
+++ scripts/generate-requirements.md.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/env -S perl -p
 $title = $ARGV;
 $title =~ s=.*?requirements/(.*?)\.txt=$1=;
+if (m(^[^#].*://)) {
+    s=(^.*://)(.*)=- [$1$2](https://$2)=;
+    next;
+}
 s`^([^#\n][^\s[>=<^!^]*)`- [$1](https://pypi.org/project/$1)`;
 s/^#\s*//;
 s/^!.*/## $title/;
