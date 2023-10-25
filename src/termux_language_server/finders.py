7c8bdc192905405de8e377d6971628ffb4d82d23
Wu, Zhenyu <wuzhenyu@ustc.edu>
2023-10-25 17:33:43 +0800

    :memo: Update _metainfo.py

--- src/termux_language_server/finders.py
+++ src/termux_language_server/finders.py
@@ -343,6 +343,36 @@ class CSVFinder(UnsortedCSVFinder):
         return links
 
 
+class PackageFinder(Finder):
+    r"""Packagefinder."""
+
+    def __call__(self, uni: UNI) -> bool:
+        r"""Call.
+
+        :param uni:
+        :type uni: UNI
+        :rtype: bool
+        """
+        parent = uni.node.parent
+        if parent is None:
+            return False
+        if parent.parent is None:
+            return False
+        return (
+            uni.node.type == "word"
+            and parent.type == "array"
+            and parent.parent.type == "variable_assignment"
+            and UNI.node2text(parent.parent.children[0])
+            in [
+                "depends",
+                "optdepends",
+                "makedepends",
+                "conflicts",
+                "provides",
+            ]
+        )
+
+
 DIAGNOSTICS_FINDER_CLASSES = [
     ErrorFinder,
     MissingFinder,
