ce14bbd397e90bb8f03031cd035625487b272207
Wu, Zhenyu <wuzhenyu@ustc.edu>
2023-11-29 16:46:18 +0800

    :art: Use shfmt to format code

--- tests/build.sh
+++ tests/build.sh
@@ -25,7 +25,7 @@ termux_pkg_auto_update() {
 		termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" "${TERMUX_PKG_UPDATE_TAG_TYPE}"
 	)"
 
-	if [[ -z "${latest_tag}" ]]; then
+	if [[ -z ${latest_tag} ]]; then
 		termux_error_exit "ERROR: Unable to get tag from ${TERMUX_PKG_SRCURL}"
 	fi
 	termux_pkg_upgrade_version "0.0.0-${latest_tag}"
