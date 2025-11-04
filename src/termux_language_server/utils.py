a9d3b8540bc7c644e2137b43dcc3b66d96b8a1c4
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-11-04 12:59:41 +0800

    :wastebasket: Update deprecated query

--- src/termux_language_server/utils.py
+++ src/termux_language_server/utils.py
@@ -36,7 +36,10 @@ def get_query(name: str, filetype: str = "bash") -> Query:
             )
         ) as f:
             text = f.read()
-        QUERIES[name] = parser.language.query(text)
+        if parser.language:
+            QUERIES[name] = Query(parser.language, text)
+        else:
+            raise NotImplementedError
     return QUERIES[name]
 
 
