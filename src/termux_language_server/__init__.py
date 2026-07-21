173be0d6f71d5e070ff77cf0d1d97296541566ef
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-07-21 13:12:32 +0800

    :truck: Rename subpackage.sh

--- src/termux_language_server/__init__.py
+++ src/termux_language_server/__init__.py
@@ -2,4 +2,4 @@ r"""Provide ``__version__`` for
 `importlib.metadata.version() <https://docs.python.org/3/library/importlib.metadata.html#distribution-versions>`_.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
